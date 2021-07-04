ac27c1e7ae3306a3049e498d26307a54bd350842
DJ <dexter13576@gmail.com>
2021-07-04 18:01:05 +0900

    init commit

---
+++ TrackingFace.X/main.c
@@ -0,0 +1,20 @@
+
+#include "mcc_generated_files/mcc.h"
+
+/*
+                         Main application
+ */
+void main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+
+
+    while (1)
+    {
+        // Add your application code
+    }
+}
+/**
+ End of File
+*/
